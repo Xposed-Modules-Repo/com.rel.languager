35c35c043e1a24fc17f49b4d2b538732ed1d97a7
Ariel Tubul <avarti765@gmail.com>
2025-03-03 22:54:27 +0200

    removed code files (sources are placed elsewhere)

--- app/src/main/java/com/rel/languager/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.rel.languager
-
-object Constants {
-    const val SHARED_PREF_FILE_NAME = "languager_prefs"
-    const val PREF_APP_LANGUAGE_MAP = "pref_app_language_map"
-    const val DEFAULT_LANGUAGE = "SYS"
-}
